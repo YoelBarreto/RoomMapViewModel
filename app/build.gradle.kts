5aae1054aec078094678363d93917441596f771e
Yoel <yobelb8@gmail.com>
2024-12-04 08:45:41 +0000

    Implementadas dependencias Room + KAPT -> KSP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -40,7 +41,13 @@ android {
 }
 
 dependencies {
+    // Mapa osmdroid
     implementation(libs.osmdroid.android)
+
+    // Room
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.runtime)
+    ksp(libs.androidx.room.compiler)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
