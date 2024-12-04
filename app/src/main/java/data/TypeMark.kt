892e350f6dffe15ed649438d7a17a3b32bddacc3
Yoel <yobelb8@gmail.com>
2024-12-04 09:05:19 +0000

    Añadida la otra tabla de tipos de marcadores

---
+++ app/src/main/java/data/TypeMark.kt
@@ -0,0 +1,11 @@
+package data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "type_marks")
+data class TypeMark(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val titulo: String
+)
