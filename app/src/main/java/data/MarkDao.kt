a0676b1723154064b6b7f68c5b4b2adc883b852f
Yoel <yobelb8@gmail.com>
2024-12-04 09:08:36 +0000

    Cambios de formato de codigo

--- app/src/main/java/data/MarkDao.kt
+++ app/src/main/java/data/MarkDao.kt
@@ -6,8 +6,14 @@ import androidx.room.*
 interface MarkDao {
     @Insert
     suspend fun insert(task: Mark)
+
     @Query("SELECT * FROM marks")
     suspend fun getAllMarks(): List<Mark>
-    @Update suspend fun update(task: Mark)
-    @Delete suspend fun delete(task: Mark)
+
+    @Update
+    suspend fun update(task: Mark)
+
+    @Delete
+    suspend fun delete(task: Mark)
+
 }
