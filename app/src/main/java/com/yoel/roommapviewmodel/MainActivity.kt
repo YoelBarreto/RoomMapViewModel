09aa86fddd3d2748ca98d9fd981b0e20ed0bb81e
Yoel <yobelb8@gmail.com>
2024-12-04 09:49:07 +0000

    Manda la aplicacion a la ScreenMap

--- app/src/main/java/com/yoel/roommapviewmodel/MainActivity.kt
+++ app/src/main/java/com/yoel/roommapviewmodel/MainActivity.kt
@@ -6,34 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.yoel.roommapviewmodel.ui.theme.RoomMapViewModelTheme
+import com.yoel.roommapviewmodel.ui.ScreenMap
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RoomMapViewModelTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Surface(modifier = Modifier.fillMaxSize()) {
+                ScreenMap()
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
