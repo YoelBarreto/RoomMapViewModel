23560e6d7caa29b8ba1f42c632ce80cc1e520a08
Yoel <yobelb8@gmail.com>
2024-12-10 13:35:30 +0000

    Primera parte de inserción de marcadores

--- app/src/main/java/com/yoel/roommapviewmodel/data/AppDataBase.kt
+++ app/src/main/java/com/yoel/roommapviewmodel/data/AppDataBase.kt
@@ -41,12 +41,37 @@ abstract class AppDatabase : RoomDatabase() {
 
             val typesInserted = typeMarkDao.getAllTypeMarks().first()
 
-
             if (typesInserted.isEmpty()) {
                 types.forEach {
                     typeMarkDao.insertTypeMark(it)
                 }
             }
+
+            val typesInsertedUpd = typeMarkDao.getAllTypeMarks().first()
+
+            val marks = listOf(
+                // 1 28.956845569943642, -13.586779205792755
+                Mark(name = "Centro Comercial Deiland", x = 28.956845569943642, y = -13.586779205792755, typeMarkId = typesInsertedUpd[0].id),
+                // 2 28.95806336981142, -13.585313992621943
+                Mark(name = "Milar Lanzarote", x = 28.95806336981142, y = -13.585313992621943, typeMarkId = typesInsertedUpd[0].id),
+                // 3 28.959074717504215, -13.591601638386324
+                Mark(name = "Lidl", x = 28.959074717504215, y = -13.591601638386324, typeMarkId = typesInsertedUpd[0].id),
+                // 4 28.95951322590405, -13.592477431479727
+                Mark(name = "Mercadona Playa Honda", x = 28.95951322590405, y = -13.592477431479727, typeMarkId = typesInsertedUpd[0].id),
+                // 5 28.957169170585573, -13.599439057067102
+                Mark(name = "Hiperdino San Bartolome", x = 28.957169170585573, y = -13.599439057067102, typeMarkId = typesInsertedUpd[0].id),
+                // 6 28.954667969496054, -13.585789576718417
+                Mark(name = "Parque infantíl Los Roques", x = 28.954667969496054, y = -13.585789576718417, typeMarkId = typesInsertedUpd[1].id),
+                // 7 29.141827384708, -13.503701097981299
+                Mark(name = "Casa/Museo Cesar Manrique", x = 29.141827384708, y = -13.503701097981299, typeMarkId = typesInsertedUpd[1].id),
+                // 8 29.18467900560408, -13.50116937418045
+                Mark(name = "Mirador de Guinate", x = 29.18467900560408, y = -13.50116937418045, typeMarkId = typesInsertedUpd[1].id),
+                Mark(name = "Casa/Museo Cesar Manrique", x = 29.141827384708, y = -13.503701097981299, typeMarkId = typesInsertedUpd[1].id),
+                Mark(name = "Casa/Museo Cesar Manrique", x = 29.141827384708, y = -13.503701097981299, typeMarkId = typesInsertedUpd[1].id),
+                Mark(name = "Casa/Museo Cesar Manrique", x = 29.141827384708, y = -13.503701097981299, typeMarkId = typesInsertedUpd[1].id),
+                Mark(name = "Casa/Museo Cesar Manrique", x = 29.141827384708, y = -13.503701097981299, typeMarkId = typesInsertedUpd[1].id),
+            )
+
         }
     }
 }
