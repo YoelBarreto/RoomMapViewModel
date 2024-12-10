57f6b54d5f7ff88ee8be14e2916f38651d4deb28
Yoel <yobelb8@gmail.com>
2024-12-10 13:44:53 +0000

    Quitado formato suspend de la funcion getAllTypeMarks

--- app/src/main/java/com/yoel/roommapviewmodel/data/TypeMarkDao.kt
+++ app/src/main/java/com/yoel/roommapviewmodel/data/TypeMarkDao.kt
@@ -9,7 +9,7 @@ interface TypeMarkDao {
     suspend fun insertTypeMark(typeMark: TypeMark)
 
     @Query("SELECT * FROM type_marks")
-    suspend fun getAllTypeMarks(): Flow<List<TypeMark>>
+    fun getAllTypeMarks(): Flow<List<TypeMark>>
 
     @Query("SELECT name FROM type_marks WHERE id = :id")
     suspend fun getTitleById(id: Int): String?
