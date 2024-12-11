dfd09b3346284fb5435d996be104d852e71d4f5d
Yoel <yobelb8@gmail.com>
2024-12-11 08:27:31 +0000

    Uso del ViewModel

---
+++ app/src/main/java/viewmodel/ViewModel.kt
@@ -0,0 +1,12 @@
+package viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.yoel.roommapviewmodel.data.MarkDao
+import com.yoel.roommapviewmodel.data.MarkWithTypeMark
+import kotlinx.coroutines.flow.Flow
+
+class MarKViewModel(private val markDao: MarkDao) : ViewModel() {
+
+    val marcadoresWithTipo: Flow<List<MarkWithTypeMark>> =
+        markDao.getAllMarksAndTypes()
+}
